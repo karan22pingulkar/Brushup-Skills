b0e17c47a19f7927ff763ff0ddd385f519aa2b46
karan22pingulkar <karan22pingulkar@gmail.com>
2026-06-01 12:13:21 +0530

    done some concepts

--- python/basics.py
+++ python/basics.py
@@ -45,3 +45,93 @@ for i in nums:
 print(count)
 print("--------------------------------------------------- % ")
 # Find the largest number:
+nums = [5, 2, 8, 1, 9, 6]
+large = nums[0]
+
+for num in nums:
+    if num > large:
+        large = num
+
+print(large)
+
+print("--------------------------------------------------- % ")
+# Problem 1: Smallest Number
+nums = [5, 2, 8, 1, 9, 6]
+small = nums[0]
+for num in nums:
+    if num < small:
+        small = num
+
+print(small)
+print("--------------------------------------------------- % ")
+# Problem 2: Count Even Numbers
+nums = [5, 2, 8, 1, 9, 6]
+count_even = 0
+for num in nums:
+    if num % 2 == 0:
+        count_even += 1
+
+print(count_even)
+print("--------------------------------------------------- % ")
+
+# Problem 3: Sum of List
+nums = [5, 2, 8, 1, 9, 6]
+list_sum = 0
+for num in nums:
+    list_sum = num + list_sum
+print(list_sum)
+# tip:accumulator = accumulator + value somtimes we can come across accumulator =  value +  accumulator  in reverse string ig
+
+print("--------------------------------------------------- % ")
+# Problem 4: Find if Number Exists
+nums = [5, 2, 8, 1, 9, 6]
+output = 8
+
+if output in nums:
+    print(f"{output} exists")
+else:
+    print(f"{output} doesnt exists")
+
+# 2 method
+print("--------------------------------------------------- % ")
+nums = [5, 2, 8, 1, 9, 6]
+output = 8
+found = False  # Start by assuming it is not there
+
+for num in nums:
+    if num == output:
+        found = True
+        break  # Stop searching immediately once found
+
+if found:
+    print(f"{output} exists")
+else:
+    print(f"{output} doesnt exists")
+
+# ✅ Variables
+
+# ✅ Arithmetic operators
+
+# ✅ Comparison operators
+
+# ✅ if / elif / else
+
+# ✅ for loops
+
+# ✅ while loops
+
+# ✅ Lists
+
+# ✅ Traversing a list
+
+# ✅ Sum pattern
+
+# ✅ Count pattern
+
+# ✅ Maximum pattern
+
+# ✅ Minimum pattern
+
+# ✅ Linear Search
+
+# ✅ break
