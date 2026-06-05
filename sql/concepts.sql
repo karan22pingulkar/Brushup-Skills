029332cd4966beebe5975e6080b321ef3dbb7243
karan22pingulkar <karan22pingulkar@gmail.com>
2026-06-05 20:22:10 +0530

    Sql added changes

--- sql/concepts.sql
+++ sql/concepts.sql
@@ -1,2 +1,6 @@
 -- char_length('🔥') = 1 (Because it is 1 character to a human)
 -- length('🔥') = 4 (Because it takes 4 bytes of computer storage)
+
+
+select city, length(city) from station order by length(city) asc, city asc limit 1;
+select city, length(city) from station order by length(city) desc, city asc limit 1;
