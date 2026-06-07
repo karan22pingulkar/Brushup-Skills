28f761393c094c5f91dd681bd178f6f8f6b3c158
karan22pingulkar <karan22pingulkar@gmail.com>
2026-06-07 12:48:52 +0530

    solved problems

--- sql/concepts.sql
+++ sql/concepts.sql
@@ -28,4 +28,8 @@ WHERE city NOT LIKE 'a%'
   AND city NOT LIKE 'o%' 
   AND city NOT LIKE 'u%';
 
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 
+SELECT name from students where 
+marks > 75 ORDER BY
+right (name,3)  ASC,id ASC
