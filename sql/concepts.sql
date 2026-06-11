6fcf7531033d17d361cc6fad14e385808de08282
karan22pingulkar <karan22pingulkar@gmail.com>
2026-06-11 16:07:21 +0530

    placement problem solved

--- sql/concepts.sql
+++ sql/concepts.sql
@@ -139,3 +139,20 @@ ORDER BY
     GRADES.GRADE DESC, 
     STUDENT_NAME ASC, 
     STUDENTS.MARKS ASC;
+
+--PLacements problem hackerrank medium
+-- You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID (ID of the ONLY best friend). Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+-- Write a query to output the names of those students whose best friends got offered a higher salary than them. Names must be ordered by the salary amount offered to the best friends. It is guaranteed that no two students got same salary offer.
+/*
+Enter your query here.
+*/
+SELECT s.name
+from students s
+INNER join friends f
+on s.id=f.id
+inner join packages  as s_salary
+on s.id=s_salary.id
+inner join packages as f_salary
+on f.friend_id=f_salary.id
+where f_salary.salary > s_salary.salary
+ORDER by f_salary.salary ASC
