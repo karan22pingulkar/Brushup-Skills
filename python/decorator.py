f2e63b5ed91d28ca5d863ce4d5adeae8ec06cc26
karan22pingulkar <karan22pingulkar@gmail.com>
2026-07-06 21:25:32 +0530

    practice decorators and learning new things

--- python/decorator.py
+++ python/decorator.py
@@ -15,3 +15,20 @@ def simple_function():
 
 
 simple_function()
+
+
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with arguments {args}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} finished executing.")
+        return result
+    return wrapper
+
+
+@log_decorator
+def add_numbers(a, b):
+    return a + b
+
+
+print(add_numbers(5, 10))
