32ce79aa39698a723389a2c430b483dfeb5c548d
karan22pingulkar <karan22pingulkar@gmail.com>
2026-07-06 21:17:33 +0530

    practice decorators and learning new things

--- python/decorator.py
+++ python/decorator.py
@@ -3,9 +3,9 @@
 # A decorator is simply a function that takes another function as an argument and returns a new, modified function.
 def simple_decorator(func):
     def wrapper():
-        print("before function is called")
+        print("before function is called this line is coming from decorator function")
         func()
-        print("after function is called")
+        print("after function is calledthis line is coming from decorator function")
     return wrapper
 
 
